dc90bccdad424d3829a0db16e5743a1fd0c29f93
hongsewi <hongsewi@snu.ac.kr>
2026-05-31 19:10:17 +0900

    Update Knitstamp Input Controller
    change mouthOpenIgnoreChange

--- KnitstampInputController.js
+++ KnitstampInputController.js
@@ -8,7 +8,7 @@ let expressionTuning = {
   eyeMaxChange: 0.035,
   browMaxChange: 8,
   mouthCurveMaxChange: 0.035,
-  mouthOpenIgnoreChange: 0.08,
+  mouthOpenIgnoreChange: 0.12,
   neutralThreshold: 0.08,
   tagThreshold: 0.16
 };
